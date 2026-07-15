01951a2801248a4360d4fda237982cd7cb04429e
Damarla Akshaya Naga Sai Kalyan <akshaydamarla1@gmail.com>
2026-07-16 00:23:38 +0530

    Time: 24 ms (31.93%), Space: 47.1 MB (17.69%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,27 +1,25 @@
 class Solution {
     public String longestPalindrome(String s) {
-        String res = "";
+        String result = "";
         for(int i=0;i<s.length();i++){
-            int l = i,r = i;
+            int l = i,r=i;
             while(l>=0&&r<s.length()&&s.charAt(l)==s.charAt(r)){
-                if(r-l+1>res.length()){
-                    res = s.substring(l,r+1);
+                if(r-l+1>result.length()){
+                    result = s.substring(l,r+1);
                 }
                 l--;
                 r++;
             }
-
-            l = i;r= i+1;
+            l=i;r=i+1;
             while(l>=0&&r<s.length()&&s.charAt(l)==s.charAt(r)){
-                if(r-l+1>res.length()){
-                    res=s.substring(l,r+1);
+                if(r-l+1>result.length()){
+                    result = s.substring(l,r+1);
                 }
                 l--;
                 r++;
             }
-
         }
-        return res;
+        return result;
         
     }
 }
