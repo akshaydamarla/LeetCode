3d075e1e3b939d7febc390d643afa3da01cb28a2
Damarla Akshaya Naga Sai Kalyan <akshaydamarla1@gmail.com>
2026-08-06 22:08:09 +0530

    Time: 213 ms (5%), Space: 85.1 MB (97.79%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.java
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -5,9 +5,14 @@ class Solution {
         }
         int maxGap = Integer.MIN_VALUE;
         int gap = Integer.MIN_VALUE;
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            gap = Math.abs(nums[i]-nums[i+1]);
+        Queue<Integer> queue = new PriorityQueue<>();
+        for(int i=0;i<nums.length;i++){
+            queue.offer(nums[i]);
+        }
+        while(queue.size()>1){
+            int n1 = queue.poll();
+            int n2 = queue.peek();
+            gap = Math.abs(n1-n2);
             maxGap = Math.max(gap,maxGap);
         }
         maxGap = Math.max(gap,maxGap);
