c42e1fdc45e48cd95753300d612741afaf7e60d7
Damarla Akshaya Naga Sai Kalyan <akshaydamarla1@gmail.com>
2026-09-01 18:55:54 +0530

    Time: 11 ms (29.18%), Space: 47.9 MB (30.29%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.java
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.java
@@ -1,15 +1,11 @@
 class Solution {
     public boolean doesAliceWin(String s) {
-        int cnt = 0;
         String vowel = "aeiou";
         for(int i=0;i<s.length();i++){
             if(vowel.indexOf(s.charAt(i))!=-1){
-                cnt++;
+                return true;
             }
         }
-        if(cnt==0){
-            return false;
-        }
-        return true;
+        return false;
     }
 }
