dae01af879ffe71028b61830abaa6f8e9c9b405e
Damarla Akshaya Naga Sai Kalyan <akshaydamarla1@gmail.com>
2026-09-15 20:50:57 +0530

    Time: 717 ms (26.18%), Space: 80 MB (7.27%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] prime = new boolean[n+1];
+        Arrays.fill(prime,true);
+        int cnt = 0;
+        if(n<2){
+            return 0;
+        }
+        prime[0] = false;
+        prime[1]=false;
+        for(int i=2;i*i<=n;i++){
+            if(prime[i]){
+                for(int j=i*i;j<=n;j+=i){
+                    prime[j]=false;
+                }
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            if(prime[i]){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
